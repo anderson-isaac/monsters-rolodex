eea276f31f70197a71c5bded08d41fa66a06a1cc
Anderson Isaac <anderson.rodz@gmail.com>
2020-01-06 02:21:27 -0300

    Updates

---
+++ precache-manifest.f161c9bcdb1979b4a15a304b98319876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9cb7d1eefc2a11e3a05c35fce5be84",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "75b21bf459de2ec97200",
+    "url": "/monster-rolodex/static/css/2.247797c6.chunk.css"
+  },
+  {
+    "revision": "8e5298d4949b1239e104",
+    "url": "/monster-rolodex/static/css/main.023c1925.chunk.css"
+  },
+  {
+    "revision": "75b21bf459de2ec97200",
+    "url": "/monster-rolodex/static/js/2.14520021.chunk.js"
+  },
+  {
+    "revision": "8e5298d4949b1239e104",
+    "url": "/monster-rolodex/static/js/main.4f0eb06d.chunk.js"
+  },
+  {
+    "revision": "ec2849d1eae557c66f4a",
+    "url": "/monster-rolodex/static/js/runtime-main.b891af59.js"
+  }
+]);
